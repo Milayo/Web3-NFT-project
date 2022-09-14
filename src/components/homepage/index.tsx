7dc4c255eff1ad590afe649f5ff5493ac304f6bd
Ijarotimi Tomilayo <moladeijarotimi@gmail.com>
2022-09-14 16:21:58 +0100

    homepage

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -1,11 +1,16 @@
 import styles from "./homepage.module.scss"
+import Image from "next/image";
+import HomeImage from "../../assets/cryptocurrency-art.png";
 
 const HomePage = () => {
   return (
-      <div className={styles.homepage}>
-          ,jkbjyu
+    <div className={styles.homepage}>
+      <div className={styles.hometitle}>
+        <p>Discover and Manage your NFTs Collections with Seer.</p>
+      </div>
+      <Image src={HomeImage} alt="HomeImage" width={500} height={450}/>
     </div>
-  )
+  );
 }
 
 export default HomePage
